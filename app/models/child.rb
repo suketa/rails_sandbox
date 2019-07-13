db1393604b50fe218c7841fc82f10232dba38061
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2019-07-13 20:00:47 +0900

    try055
    Guard Enums against definitions with blank label names
    https://github.com/rails/rails/pull/34385

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,4 +1,3 @@
 class Child < ApplicationRecord
-  enum generation: [baby: 0, toddler: 1, preschool: 2, gradeschool: 3, teen: 4, young_adult: 5]
-  # enum generation: { baby: 0, toddler: 1, preschool: 2, gradeschool: 3, teen: 4, young_adult: 5 }
+  enum generation: { '' => 0, toddler: 1, preschool: 2, gradeschool: 3, teen: 4, young_adult: 5 }
 end
