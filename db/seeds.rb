b2cc41b5ddc824c89c309caa8a78902a6e5816ea
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2019-08-17 09:55:20 +0900

    try076
    Introduce delete_by and destroy_by methods to ActiveRecord::Relation
    https://github.com/rails/rails/pull/35316

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Library.create(
+  [
+    {
+      name: 'Tokyo',
+      books: Book.create(
+        [
+          { title: 'Programming Ruby' },
+          { title: 'The Pragmatic Programmer' },
+          { title: 'Agile Web Development with Rails' }
+        ]
+      )
+    },
+    {
+      name: 'Kanagawa',
+      books: Book.create(
+        [
+          { title: 'Programming Ruby' }
+        ]
+      )
+    },
+    {
+      name: 'Chiba',
+      books: Book.create(
+        [
+          { title: 'Programming Ruby' },
+          { title: 'The Pragmatic Programmer' }
+        ]
+      )
+    }
+  ]
+)
