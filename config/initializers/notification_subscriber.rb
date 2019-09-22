893db3f03cde18be56e2887f4d362b3a661dc229
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2019-09-22 12:52:20 +0900

    try090
    Instrument middleware processing
    https://github.com/rails/rails/commit/04ae0b0b5e594e0bb99c5cd608921745977bcdcd

--- config/initializers/notification_subscriber.rb
+++ config/initializers/notification_subscriber.rb
@@ -1,5 +1,3 @@
-ActiveSupport::Notifications.subscribe 'process_action.action_controller' do |event|
-  action = "#{event.payload[:controller]}##{event.payload[:action]}"
-  Rails.logger.info("event.class=#{event.class}")
-  Rails.logger.info("#{action} cpu_time=#{event.cpu_time} duration=#{event.duration} allocations=#{event.allocations}")
+ActiveSupport::Notifications.subscribe 'process_middleware.action_dispatch' do |event|
+  Rails.logger.info(event.payload[:middleware])
 end
