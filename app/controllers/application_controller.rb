7493997bced2d3e1ab19acbec26a4767d4430169
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2019-09-22 16:07:40 +0900

    try091
    Add support for annotating queries generated by ActiveRecord::Relation with SQL comments
    https://github.com/rails/rails/pull/35617

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def full_action_name
+    "#{self.class.name}##{action_name}"
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  scope :called_from, ->(from) { annotate("called from #{from}") }
 end
