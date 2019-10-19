03ad43d2350b7366e1d66f8e977f897f2e8e3675
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2019-10-19 14:38:47 +0900

    try104
    Extract default log file open operation to method
    https://github.com/rails/rails/pull/36407

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :reopen_log
+
+  def reopen_log
+    Rails.logger.reopen Rails.configuration.default_log_file unless File.exist?(Rails.configuration.paths['log'].first)
+  end
 end
