e6b9001c6e8ba6d1538985da102e46b8bd81ad21
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-11-02 13:51:52 +0900

    order and match params

--- sandbox/app/controllers/users_controller.rb
+++ sandbox/app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @id_order = params[:id_order] || "asc"
+    @users = User.search(name: params[:name], id_order: params[:id_order])
   end
 
   # GET /users/1 or /users/1.json
--- sandbox/app/models/user.rb
+++ sandbox/app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   normalizes :email, with: ->(email) { email.strip.downcase }
+
+  scope :much_name, ->(name) { where("name ILIKE ?", "%#{name}%") }
+  scope :ordered_by_id, ->(id_order) { order(id: Arel.sql(id_order)) }
+
+  class << self
+    def search(name: nil, id_order: "asc")
+      id_order = id_order.presence_in(%w[asc desc]) || "asc"
+      users = all
+      users = users.much_name(name) if name.present?
+      users = users.ordered_by_id(id_order)
+      users
+    end
+  end
 end
