d509bdf5e39530214dbd67f169017beda9186009
Masaki Suketa <m-suketa@ruby-dev.jp>
2025-12-24 15:22:44 +0900

    add confirm dialog
    https://boringrails.com/articles/data-turbo-confirm-beautiful-dialog/

--- sandbox/app/javascript/controllers/index.js
+++ sandbox/app/javascript/controllers/index.js
@@ -2,3 +2,26 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+function initializeConfirmDialog() {
+  Turbo.config.forms.confirm = (message, element, submitter) => {
+    const confirmDialog = document.getElementById('turbo-confirm-dialog');
+    const confirmMessage = document.getElementById('turbo-confirm-message');
+    const confirmButton = confirmDialog?.querySelector('button[value="confirm"]');
+
+    if (!confirmDialog) return Promise.resolve(confirm(message));
+
+    confirmMessage.textContent = message;
+    const buttonText = submitter?.dataset?.turboConfirmButton || 'OK';
+    confirmButton.textContent = buttonText;
+
+    confirmDialog.showModal();
+
+    return new Promise((resolve) => {
+      confirmDialog.addEventListener('close', () => {
+        resolve(confirmDialog.returnValue === 'confirm');
+      }, { once: true });
+    });
+  }
+}
+initializeConfirmDialog();
