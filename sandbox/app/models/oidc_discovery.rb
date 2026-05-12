51aae49dc3cfd09ac80be53900884705f89bd258
Masaki Suketa <m-suketa@ruby-dev.jp>
2026-05-12 13:51:33 +0900

    refactoring

--- sandbox/app/models/oidc_discovery.rb
+++ sandbox/app/models/oidc_discovery.rb
@@ -1,43 +1,45 @@
 # frozen_string_literal: true
 
 class OidcDiscovery
-  class << self
-    def config
-      Rails.cache.fetch("oidc:discovery:#{Settings.oidc_issuer}", expires_in: 1.hour) do
-        result = OpenIDConnect::Discovery::Provider::Config.discover!(Settings.oidc_issuer)
-        {
-          authorization_endpoint: result.authorization_endpoint,
-          token_endpoint: result.token_endpoint,
-          userinfo_endpoint: result.userinfo_endpoint,
-          jwks_uri: result.jwks_uri,
-          issuer: result.issuer,
-          pushed_authorization_request_endpoint: result.raw['pushed_authorization_request_endpoint'],
-        }
-      end
-    end
+  def initialize(issuer:)
+    @issuer = issuer
+  end
 
-    def authorization_endpoint
-      config[:authorization_endpoint]
+  def config
+    Rails.cache.fetch("oidc:discovery:#{@issuer}", expires_in: 1.hour) do
+      result = OpenIDConnect::Discovery::Provider::Config.discover!(@issuer)
+      {
+        authorization_endpoint: result.authorization_endpoint,
+        token_endpoint: result.token_endpoint,
+        userinfo_endpoint: result.userinfo_endpoint,
+        jwks_uri: result.jwks_uri,
+        issuer: result.issuer,
+        pushed_authorization_request_endpoint: result.raw['pushed_authorization_request_endpoint'],
+      }
     end
+  end
 
-    def token_endpoint
-      config[:token_endpoint]
-    end
+  def authorization_endpoint
+    config[:authorization_endpoint]
+  end
 
-    def userinfo_endpoint
-      config[:userinfo_endpoint]
-    end
+  def token_endpoint
+    config[:token_endpoint]
+  end
 
-    def jwks_uri
-      config[:jwks_uri]
-    end
+  def userinfo_endpoint
+    config[:userinfo_endpoint]
+  end
 
-    def issuer
-      config[:issuer]
-    end
+  def jwks_uri
+    config[:jwks_uri]
+  end
 
-    def pushed_authorization_request_endpoint
-      config[:pushed_authorization_request_endpoint]
-    end
+  def issuer
+    config[:issuer]
+  end
+
+  def pushed_authorization_request_endpoint
+    config[:pushed_authorization_request_endpoint]
   end
 end
