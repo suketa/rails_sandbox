d9c4a1959d031d740b2e9efbb72a3d2a21c2c940
Masaki Suketa <m-suketa@ruby-dev.jp>
2026-05-21 08:33:33 +0900

    OidcUserInfo error handling

--- sandbox/app/models/oidc_user_info.rb
+++ sandbox/app/models/oidc_user_info.rb
@@ -1,18 +1,35 @@
 # frozen_string_literal: true
 
 class OidcUserInfo
+  class UserInfoEndpointError < OidcError; end
+  class UserInfoResponseParseError < OidcError; end
+
   def initialize(discovery:, access_token:)
     @discovery = discovery
     @access_token = access_token
   end
 
   def fetch
+    body = fetch_userinfo_response
+    JSON.parse(body, symbolize_names: true)
+  rescue JSON::ParserError
+    raise UserInfoResponseParseError, "failed to parse response body"
+  end
+
+  private
+
+  def fetch_userinfo_response
     uri = URI.parse(@discovery.userinfo_endpoint)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = (uri.scheme == "https")
     req = Net::HTTP::Get.new(uri.path)
     req["Authorization"] = "Bearer #{@access_token}"
     res = http.request(req)
-    JSON.parse(res.body, symbolize_names: true)
+    case res
+    when Net::HTTPSuccess
+      res.body
+    else
+      raise UserInfoEndpointError, "status=#{res.code}"
+    end
   end
 end
