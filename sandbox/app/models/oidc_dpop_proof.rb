3802212554975f62fca0e0cff40e26c80733ff6d
Masaki Suketa <m-suketa@ruby-dev.jp>
2026-05-26 12:11:03 +0900

    OidcDpopProof ath, nonce

--- sandbox/app/models/oidc_dpop_proof.rb
+++ sandbox/app/models/oidc_dpop_proof.rb
@@ -3,15 +3,24 @@
 class OidcDpopProof
   ALG = :ES256
 
-  def initialize(signing_jwk:, htm:, htu:)
+  def initialize(signing_jwk:, htm:, htu:, ath: nil, nonce: nil)
     @signing_jwk = signing_jwk
     @htm = htm
     @htu = htu
+    @ath = ath
+    @nonce = nonce
   end
 
   def to_jwt
     payload = {
+      htm: @htm,
+      htu: @htu,
+      jti: SecureRandom.uuid,
+      iat: Time.current.to_i,
     }
+    payload = payload.merge(ath: @ath) if @ath.present?
+    payload = payload.merge(nonce: @nonce) if @nonce.present?
+
     JSON::JWT.new(payload).tap do |j|
       j.header[:typ] = "dpop+jwt"
       j.header[:jwk] = @signing_jwk.normalize
--- sandbox/spec/models/oidc_dpop_proof_spec.rb
+++ sandbox/spec/models/oidc_dpop_proof_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe OidcDpopProof do
     it "typ == 'dpop+jwt' / alg == ES256 がヘッダにあること" do
       proof = described_class.new(signing_jwk:, htm: "POST", htu: "http://example.com")
       jwt = proof.to_jwt
-
       decoded = JSON::JWT.decode(jwt, signing_jwk)
+
       expect(decoded.header[:typ]).to eq("dpop+jwt")
       expect(decoded.header[:alg]).to eq("ES256")
     end
@@ -22,10 +22,55 @@ RSpec.describe OidcDpopProof do
     it "jwk に d が無いこと" do
       proof = described_class.new(signing_jwk:, htm: "POST", htu: "http://example.com")
       jwt = proof.to_jwt
-
       decoded = JSON::JWT.decode(jwt, signing_jwk)
       jwk_header = decoded.header[:jwk]
+
       expect(jwk_header.keys.map(&:to_s)).not_to include("d")
+    end
+
+    it "claims に htm, htu, iat, jti がある" do
+      proof = described_class.new(signing_jwk:, htm: "POST", htu: "http://example.com")
+      jwt = proof.to_jwt
+      decoded = JSON::JWT.decode(jwt, signing_jwk)
+
+      expect(decoded[:htm]).to eq("POST")
+      expect(decoded[:htu]).to eq("http://example.com")
+      expect(decoded[:iat]).to eq(Time.current.to_i)
+      expect(decoded[:jti]).to be_a(String).and be_present
+    end
+
+    it "jti が2回の生成で異なる" do
+      proof = described_class.new(signing_jwk:, htm: "POST", htu: "http://example.com")
+      jwt1 = proof.to_jwt
+      decoded1 = JSON::JWT.decode(jwt1, signing_jwk)
+      jwt2 = proof.to_jwt
+      decoded2 = JSON::JWT.decode(jwt2, signing_jwk)
+
+      expect(decoded1[:jti]).not_to eq(decoded2[:jti])
+    end
+
+    it "ath 指定なら claim に含まれ、指定しなければ含まれない" do
+      proof = described_class.new(signing_jwk:, htm: "POST", htu: "http://example.com", ath: "the-ath")
+      jwt = proof.to_jwt
+      decoded = JSON::JWT.decode(jwt, signing_jwk)
+      expect(decoded[:ath]).to eq("the-ath")
+
+      proof = described_class.new(signing_jwk:, htm: "POST", htu: "http://example.com")
+      jwt = proof.to_jwt
+      decoded = JSON::JWT.decode(jwt, signing_jwk)
+      expect(decoded.key?(:ath)).to be(false)
+    end
+
+    it "nonce 指定なら claim に含まれ、指定しなければ含まれない" do
+      proof = described_class.new(signing_jwk:, htm: "POST", htu: "http://example.com", nonce: "the-nonce")
+      jwt = proof.to_jwt
+      decoded = JSON::JWT.decode(jwt, signing_jwk)
+      expect(decoded[:nonce]).to eq("the-nonce")
+
+      proof = described_class.new(signing_jwk:, htm: "POST", htu: "http://example.com")
+      jwt = proof.to_jwt
+      decoded = JSON::JWT.decode(jwt, signing_jwk)
+      expect(decoded.key?(:nonce)).to be(false)
     end
   end
 end
